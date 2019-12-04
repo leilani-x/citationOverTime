11594695bd693c1c1d76404785e6021f7aa894f7
leilanxin <lei@IT-C02Z60UNLVDL.local>
2019-12-04 18:33:46 +0800

    extract number

--- keywordPaperNum.py
+++ keywordPaperNum.py
@@ -8,7 +8,7 @@ import time, os
 
 ## Paper setting ##################################################################
 
-paperID = 14537753363344344488  # TODO auto-get by paper name 
+keyword = "deep+learning"  # TODO auto-get by paper name 
 # please get in google scholar
 
 startYear = 1999                # TODO set default to the year when paper published
@@ -16,8 +16,8 @@ endYear = 2019                  # TODO set default to current year
 interval = 1 
 
 ## Basic setting ##################################################################
-userProfile = r"C:\Users\yourName\AppData\Local\Google\Chrome\User Data\Default"
-chromedriverPath = r"D:\chromedriver_win32\chromedriver.exe"
+#userProfile = r"C:\Users\yourName\AppData\Local\Google\Chrome\User Data\Default"
+chromedriverPath = r"/Users/lei/Downloads/chromedriver"
 ###################################################################################
 
 chrome_options = webdriver.ChromeOptions()
@@ -25,17 +25,25 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("start-maximized")
-chrome_options.add_argument("user-data-dir={}".format(userProfile))
+#chrome_options.add_argument("user-data-dir={}".format(userProfile))
 
 driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chromedriverPath)
 basicUrl = 'https://scholar.google.com/scholar?hl=en&as_sdt=0%2C5&sciodt=0%2C5&cites='
 
+print("==========keyword:",keyword)
+
 for i in range(startYear, endYear+1, interval):
-    finalUrl = basicUrl + str(paperID) + '&scipsc=&as_ylo=' + str(i) + '&as_yhi=' + str(i+interval-1)
+    finalUrl = "https://scholar.google.com.hk/scholar?hl=en&as_sdt=1%2C5&as_ylo=" + str(i) + '&as_yhi=' + str(i+interval-1) + "&as_vis=1&q=" + str(keyword) + "&btnG="
     driver.get(finalUrl)
     element = driver.find_element_by_id('gs_ab_md')
-    test = driver.find_elements_by_class_name('gs_ab_mdw')
-    print(i,'-',element.text) # TODO extract number 
+    driver.find_elements_by_class_name('gs_ab_mdw')
+    data = element.text
+    if data.split()[0] != 'about':
+        number = data.split()[0]
+    else:
+        number = data.split()[1]
+    number = element.text.split()[1]
+    print('year: {}, about {} results'.format(i, number)) 
     time.sleep(2)
 
 # TODO save numbers in scv and plot figure 
